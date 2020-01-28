75deb54d1cd6cc37c88d541aa90c1258755b888d
AbhishekSanthoshJaya <AbhishekSanthoshJaya@users.noreply.github.com>
2020-01-28 15:50:33 -0500

    Added class exercise

--- src/main/java/StringManipulationExample.java
+++ src/main/java/StringManipulationExample.java
@@ -1,18 +1,21 @@
+import java.util.Scanner;
+
 public class StringManipulationExample {
     public static void main(String[] args) {
         StringManipulationExample s1 = new StringManipulationExample();
-        String[] mainString = new String[]{"Canada", "India", "Abhishek", "Toronto","North York"};
+        String[] mainString = new String[]{"Canada", "India", "Abhishek", "Toronto", "North York"};
         for (int i = 0; i < mainString.length; i++) {
-         // System.out.println(s1.zigZagReverse(mainString[i]));
+            // System.out.println(s1.zigZagReverse(mainString[i]));
             //System.out.println(s1.zigZagMyString(mainString[i]));
             //System.out.println(s1.oddEvenSwap(mainString[i]));
-           System.out.println(s1.myReverse(mainString[i]));
+            //System.out.println(s1.myReverse(mainString[i]));
+            s1.firstDay();
         }
     }
 
     public String reverseString(String revString) {
         char[] revStore = revString.toCharArray();
-        for (int i = 0, j = revString.length()- 1; i < revString.length() / 2; i++,j--) {
+        for (int i = 0, j = revString.length() - 1; i < revString.length() / 2; i++, j--) {
             char t = revStore[i];
             revStore[i] = revStore[j];
             revStore[j] = t;
@@ -39,7 +42,7 @@ public class StringManipulationExample {
                 String firstPart2 = firstPart.substring(i, j + 1);
                 finalString1 += reverseString(firstPart2);
             }
-            for (int i = 0, j = i + 1; i < secondPart.length()-1; i += 2, j += 2) {
+            for (int i = 0, j = i + 1; i < secondPart.length() - 1; i += 2, j += 2) {
                 String secondPart2 = secondPart.substring(i, j + 1);
                 finalString2 += reverseString(secondPart2);
             }
@@ -48,22 +51,19 @@ public class StringManipulationExample {
         return finalString;
     }
 
-    public static String zigZagMyString(String s)
-    {
-        String temp ;
+    public static String zigZagMyString(String s) {
+        String temp;
         char names[] = s.toCharArray();
         char output[] = s.toCharArray();
         int len = names.length;
         int count = len - len % 2;
 
-        for(int i = 0; i < count; i+=2)
-        {
-            output[i] = names[i+1];
-            output[i+1] = names[i];
+        for (int i = 0; i < count; i += 2) {
+            output[i] = names[i + 1];
+            output[i + 1] = names[i];
         }
         temp = new String(output);
-        if (len % 2 == 1)
-        {
+        if (len % 2 == 1) {
             String firstPart = temp.substring(0, len / 2);
             //System.out.println("First Part : " + firstPart) ;
             String secondPart = temp.substring(len / 2, len - 1);
@@ -75,45 +75,59 @@ public class StringManipulationExample {
         return temp;
 
     }
+
     public String oddEvenSwap(String s) {
         char[] evens = s.toCharArray();
-      //  char[] odds = s.toCharArray();
+        //  char[] odds = s.toCharArray();
 
-        for (int i = 0; i < s.length()-1; i+=4)
-        {
-            if(i+2 < s.length()) {
+        for (int i = 0; i < s.length() - 1; i += 4) {
+            if (i + 2 < s.length()) {
                 char t = evens[i];
-                evens[i] = evens[i+2];
-                evens[i+2] = t;
+                evens[i] = evens[i + 2];
+                evens[i + 2] = t;
             }
         }
 
-        for(int j =1; j<s.length()-1;j+=4){
-            if(j+2 < s.length()) {
+        for (int j = 1; j < s.length() - 1; j += 4) {
+            if (j + 2 < s.length()) {
                 char t = evens[j];
-                evens[j] = evens[j+2];
-                evens[j+2] = t;
+                evens[j] = evens[j + 2];
+                evens[j + 2] = t;
             }
         }
         String temp = new String(evens);
         return temp;
     }
 
-    public String myReverse(String s){
+    public String myReverse(String s) {
         char[] rev = s.toCharArray();
-        for(int i = 0; i <rev.length-1; i+=2){
+        for (int i = 0; i < rev.length - 1; i += 2) {
             char t = rev[i];
-            rev[i] = rev[i+1];
-            rev[i+1] = t;
+            rev[i] = rev[i + 1];
+            rev[i + 1] = t;
         }
         String temp1 = new String(rev);
-        if(rev.length % 2 == 1){
-            String firstPart = temp1.substring(0,rev.length/2);
-            String secondPart = temp1.substring(rev.length/2, rev.length-1);
-            String finalString = firstPart + rev[rev.length-1] + secondPart;
+        if (rev.length % 2 == 1) {
+            String firstPart = temp1.substring(0, rev.length / 2);
+            String secondPart = temp1.substring(rev.length / 2, rev.length - 1);
+            String finalString = firstPart + rev[rev.length - 1] + secondPart;
             String temp2 = new String(finalString);
             return temp2;
-        }
-        else return temp1;
+        } else return temp1;
     }
-}
+
+    public void firstDay(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your string pls: ");
+        String s = input.nextLine();
+        int index = 0;
+        char[] text = s.toCharArray();
+        for(int i =1; i<text.length; i++){
+            for(int j=0; j<=i; j++){
+                System.out.print(text[index]);
+                index++;
+                }
+            }
+            System.out.println();
+        }
+    }
